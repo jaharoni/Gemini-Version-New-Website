697e66e812879de7d18864d0e2427d693010fab7
Justin <115413758+jaharoni@users.noreply.github.com>
2025-12-12 10:15:38 -0500

    feat: Migrate complete Nov12 site structure - Part 1 (core files)

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AppRouter from "./Router";
+import { CinematicBackground } from "./components/layout/CinematicBackground";
+
+function App() {
+  return (
+    <>
+      <CinematicBackground />
+      <AppRouter />
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import App from './App';
+import './index.css';
+
+if ('scrollRestoration' in window.history) {
+  window.history.scrollRestoration = 'manual';
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </HelmetProvider>
+  </React.StrictMode>
+);
